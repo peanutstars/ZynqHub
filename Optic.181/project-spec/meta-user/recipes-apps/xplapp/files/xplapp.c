4422a1ca4ae9b97a768a3b4ac08b4981eb7b4e4b
ahnfrog <ahnfrog@naver.com>
2019-03-11 22:44:29 +0900

    Updated xplapp.c.

--- Optic.181/project-spec/meta-user/recipes-apps/xplapp/files/xplapp.c
+++ Optic.181/project-spec/meta-user/recipes-apps/xplapp/files/xplapp.c
@@ -32,8 +32,22 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "xplioctl.h"
 #include "debug.h"
+
+struct Config {
+    int init;
+    int reset;
+    uint32_t vout_fd;
+    uint32_t fb_mem;
+};
+typedef struct Config Config;
 
 extern int activate_vdma_0(int base, int hsize, int vsize, uint32_t *fb_base);
 extern int activate_vdma_1(int base, int hsize, int vsize, uint32_t *vdma1_base);
@@ -44,21 +58,48 @@ extern int activate_vdma_4(int base, int hsize, int vsize, uint32_t *vdma4_base,
 void check_xpl_interface(void);
 
 
-static uint32_t _fb_mem = 0x40000000;
+void reset_fpga_core(void)
+{
+    int fd;
+    size_t vlength = 0x3000;
+    uint32_t *vaddr;
 
+    fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd < 0) {
+        fprintf(stderr, "%s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    vaddr = mmap(NULL, vlength, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000000);
 
-void initialize(uint32_t *fb_mem)
+    printf("Reset FPGA CORE, Virtual Address = %p.\n", vaddr);
+    *(vaddr + 0x1000/4) = 0x00000001;
+    *(vaddr + 0x2000/4) = 0x00000001;
+    *(vaddr + 0x1000/4) = *(vaddr + 0x1000/4) & 0xFFFFFFFE;
+    *(vaddr + 0x2000/4) = *(vaddr + 0x2000/4) & 0xFFFFFFFE;
+    
+    munmap(vaddr, vlength);
+}
+
+void initialize(Config *cfg)
 {
     xpl_open();
     printf("xpl_open OK\n");
     check_xpl_interface();
-    printf("check xpl interface OK\n");
+    printf("FB memory address: 0x%08X\n", cfg->fb_mem);
 
-    // activate_vdma_0(0, 1920, 1080, fb_mem);
-	activate_vdma_1(0, 1920, 1080, fb_mem);
-	// activate_vdma_2(0, 1920, 1080, fb_mem);
-	activate_vdma_3(0, 1920, 1080, fb_mem);
-	activate_vdma_4(0, 1920, 1080, fb_mem, 0);
+    if (cfg->reset) {
+        reset_fpga_core();
+    }
+    if (cfg->init) {
+        // activate_vdma_0(0, 1920, 1080, fb_mem);
+    	activate_vdma_1(0, 1920, 1080, &cfg->fb_mem);
+    	// activate_vdma_2(0, 1920, 1080, fb_mem);
+    	activate_vdma_3(0, 1920, 1080, &cfg->fb_mem);
+    	activate_vdma_4(0, 1920, 1080, &cfg->fb_mem, 0);
+    }
+    else {
+    	activate_vdma_3(0, 1920, 1080, &cfg->fb_mem);
+    }
 }
 
 void finalize(void)
@@ -83,20 +124,71 @@ void check_xpl_interface(void)
     exit(1);
 }
 
+void usage(void)
+{
+    const char *doc = \
+    "  usage: xplinit [-i] [-d <fb_num>]i [-r]\n" \
+    "    options:\n" \
+    "      -i             do initialization.\n" \
+    "      -d <fb_num>    set fb to oup video, fb_num is range 0 to 7.\n" \
+    "      -r             reset fpga core.\n" \
+    "\n";
+
+    fprintf(stderr, "%s", doc);
+    exit(EXIT_FAILURE);
+}
+
+int options(Config *cfg, int argc, char **argv)
+{
+    int opt;
+    uint32_t _fb_mem[8] = {\
+        0x40000000, 0x40800000,
+        0x41000000, 0x41800000,
+        0x42000000, 0x42800000,
+        0x43000000, 0x43800000,
+    };
+    
+
+    while ((opt = getopt(argc, argv, "hid:")) != -1) {
+        switch (opt) {
+            case 'h':
+                usage();
+                break;
+            case 'i':
+                cfg->reset = 1;
+                cfg->init = 1;
+                break;
+            case 'd':
+                cfg->vout_fd = (uint32_t) atoi(optarg);
+                break;
+            case 'r':
+                cfg->reset = 1;
+                break;
+            default: /* '?' */
+                usage();
+        }
+    }
+    if (cfg->vout_fd >= 8) {
+        fprintf(stderr, "FB port is ranged from 0 to 7.\n");
+        usage();
+    }
+    cfg->fb_mem = _fb_mem[cfg->vout_fd];
+
+    fprintf(stderr, "Options, reset:%d init:%d video_out_fd:%d\n", 
+            cfg->reset, cfg->init, cfg->vout_fd);
+
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
-    uint32_t *fb_mem = &_fb_mem;
-    uint32_t addr;
+    Config cfg = { 0, };
+    
+    options(&cfg, argc, argv);
 
 	printf("VDMA & FB Test Program - Optilogic a %s %s\n", __DATE__, __TIME__);
-    if (argc >= 2) {
-        addr = strtoul(argv[1], NULL, 0);
-        fb_mem = &addr;
-    }
 
-    initialize(fb_mem);
-
-    printf("Set Memory Address of Frame Buffer: 0x%08X\n", *fb_mem);
+    initialize(&cfg);
 
     finalize();
     return 0;
--- Optic.181/project-spec/meta-user/recipes-kernel/linux/linux-xlnx_%.bbappend
+++ Optic.181/project-spec/meta-user/recipes-kernel/linux/linux-xlnx_%.bbappend
@@ -1,5 +1,7 @@
 SRC_URI += "file://user_2018-09-18-22-16-00.cfg \
             file://user_2018-09-28-22-10-00.cfg \
+            file://user_2019-03-11-22-09-00.cfg \
+            file://user_2019-03-11-22-29-00.cfg \
             "
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
